f047a9d8dbef43abcaf97e3c5e660acc747b10ba
OHSONGMIN <143185293+OHSONGMIN@users.noreply.github.com>
2024-02-09 02:33:15 +0900

    Feat : CommunityServiceImpl.class
    비즈니스 로직 처리를 위한 Service 생성
    CommunityService 인터페이스 구현

---
+++ DEV_QUIZ/src/main/java/com/devquiz/biz/serviceimpl/CommunityServiceImpl.java
@@ -0,0 +1,25 @@
+package com.devquiz.biz.serviceimpl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devquiz.biz.dao.CommunityDAO;
+import com.devquiz.biz.model.CategoryVO;
+import com.devquiz.biz.model.CommentVO;
+import com.devquiz.biz.model.CommunityPageVO;
+import com.devquiz.biz.model.CommunityVO;
+import com.devquiz.biz.service.CommunityService;
+
+@Service("communityService")
+public class CommunityServiceImpl implements CommunityService{
+	@Autowired
+	private CommunityDAO communityDAO;
+	
+	public CommunityServiceImpl() {
+		System.out.println(">> CommunityServiceImpl() 객체 생성");
+	}
+}
