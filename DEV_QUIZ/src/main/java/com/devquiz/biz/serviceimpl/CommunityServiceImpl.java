5d3d912f51aaa9c03773172daa91ad61ae86f277
OHSONGMIN <143185293+OHSONGMIN@users.noreply.github.com>
2024-02-11 01:03:44 +0900

    Feat : 페이징 Param 메서드(해당 카테고리 게시글)
    CommunityServiceImpl - communityPagingParamByCate

--- DEV_QUIZ/src/main/java/com/devquiz/biz/serviceimpl/CommunityServiceImpl.java
+++ DEV_QUIZ/src/main/java/com/devquiz/biz/serviceimpl/CommunityServiceImpl.java
@@ -112,4 +112,39 @@ public class CommunityServiceImpl implements CommunityService{
 		return communityPagingList;
 	}
 	
+	@Override //해당 카테고리 게시글 페이징 처리
+	public CommunityPageVO communityPagingParamByCate(int cateIdx, int page) {
+		// 해당 카테고리 게시글 개수 조회
+        int boardCount = communityDAO.communityBoardCountByCate(cateIdx);
+        
+        // 해당 카테고리 게시글 개수 계산(10/3=3.33333 => 4)
+        int maxPage = (int) (Math.ceil((double) boardCount / pageLimit));
+        
+        // 시작 페이지 값 계산(1, 4, 7, 10, ~~~~)
+        int startPage = (((int)(Math.ceil((double) page / blockLimit))) - 1) * blockLimit + 1;
+        
+        // 끝 페이지 값 계산(3, 6, 9, 12, ~~~~)
+        int endPage = startPage + blockLimit - 1;
+        if (endPage > maxPage) {
+            endPage = maxPage;
+        }
+        
+        // 이전 블록의 시작 페이지
+        int blockPreStartPage = (((int)(Math.ceil((double) page / blockLimit))) - 2) * blockLimit + 1;
+        
+        // 다음 블록의 시작 페이지
+        int blockNextStartPage = ((int)(Math.ceil((double) page / blockLimit))) * blockLimit + 1;
+        
+        CommunityPageVO vo = new CommunityPageVO();
+        vo.setPage(page);
+        vo.setMaxPage(maxPage);
+        vo.setStartPage(startPage);
+        vo.setEndPage(endPage);
+        vo.setBlockLimit(blockLimit);
+        vo.setBlockPreStartPage(blockPreStartPage);
+        vo.setBlockNextStartPage(blockNextStartPage);
+        
+        return vo;
+	}
+	
 }
