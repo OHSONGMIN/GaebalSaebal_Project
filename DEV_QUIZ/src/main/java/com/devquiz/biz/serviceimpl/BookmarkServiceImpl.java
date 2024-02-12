ec66887ee0366731429b1863ffda0e901e84ac75
OHSONGMIN <143185293+OHSONGMIN@users.noreply.github.com>
2024-02-13 00:34:08 +0900

    Feat : BookmarkServiceImpl.class
    비즈니스 로직 처리를 위한 Service 생성
    BookmarkService 인터페이스 구현

---
+++ DEV_QUIZ/src/main/java/com/devquiz/biz/serviceimpl/BookmarkServiceImpl.java
@@ -0,0 +1,25 @@
+package com.devquiz.biz.serviceimpl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devquiz.biz.dao.BookmarkDAO;
+import com.devquiz.biz.model.BookmarkVO;
+import com.devquiz.biz.model.CommunityPageVO;
+import com.devquiz.biz.model.CommunityVO;
+import com.devquiz.biz.service.BookmarkService;
+
+@Service("bookmarkService")
+public class BookmarkServiceImpl implements BookmarkService{
+		@Autowired
+		private BookmarkDAO bookmarkDAO;
+		
+		public BookmarkServiceImpl() {
+			System.out.println(">> BookmarkServiceImpl() 객체 생성");
+		}
+		
+}
